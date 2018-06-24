f0a78b5f521436daabe8868b5830843697b1ff05
Josh Cesana <joshuacesana@gmail.com>
2018-06-24 13:38:47 -0700

    Stubbing Out CLI Class

--- lib/gyms/cli.rb
+++ lib/gyms/cli.rb
@@ -1,7 +1,43 @@
 class Gyms::CLI
 
   def call
-    puts "Gym Locator - Enter zip code:"
+    get_zip
+    menu
+    goodbye 
+  end
+
+  def get_zip
+    # doc (minus zip in URL): http://www.gymsandfitnessclubs.com/gyms-by-location/results.php?postal_code=94582
+    puts ""
+    puts "Gym Locator"
+    puts ""
+    puts "Enter zip code: "
+    zip = gets.strip
+    while zip.strip.length != 5 && zip.to_i.strip.class != Integer
+      puts "Invalid entry"
+      get_zip
+    end
+  end
+
+  def menu
+    puts "Enter gym number to get more details: "
+    input = gets.strip
+
+    while input > 0 && input < 11
+      case input
+      when "1"
+        puts "More info on gym #1"
+      when "2"
+        puts "More info on gym #2"
+      else
+        puts "Invalid entry. Try again."
+        menu
+      end
+    end
+  end
+
+  def goodbye
+    "Thank you for using Gyms!"
   end
 
 end
